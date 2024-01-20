8f47ca0c4ec76ae0d8e3e13f6574d4ba19390256
avlgal <130287958+avlgal@users.noreply.github.com>
2024-01-20 15:45:57 +0500

    Add files via upload

---
+++ Новая папка/script.js
@@ -0,0 +1,23 @@
+let name = prompt("Ваше имя?", "");
+
+document.querySelector('.name').innerHTML += name;
+
+let surname = prompt("Ваша фамилия?", "");
+
+document.querySelector('.surname').innerHTML += surname;
+
+let year = prompt("Ваш возраст?", "");
+
+document.querySelector('.year').innerHTML += year;
+
+let town = prompt("Ваш город?", "");
+
+document.querySelector('.town').innerHTML += town;
+
+let country = prompt("Ваша страна?", "");
+
+document.querySelector('.country').innerHTML += country;
+
+let birthdate = prompt("Ваша дата рождения?", "");
+
+document.querySelector('.birthdate').innerHTML += birthdate;
